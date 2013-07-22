32c80e687bd0a85fca4fdff3924fba891d4be9bf
vitoshacademy <vitosh@abv.bg>
2013-07-22 20:49:50 +0300

    Create 05. Faktoriel2.cs

---
+++ 05. Faktoriel2.cs
@@ -0,0 +1,27 @@
+//Write a program that calculates N!*K! / (K-N)! for given N and K (1<N<K).
+using System;
+using System.Collections.Generic;
+
+class CalculatingFactorialFormula2
+{
+    static decimal CalculateFactoriel(int number)
+    {
+        decimal factoriel = 1M;
+        for (int i = 1; i <= number; i++)
+        {
+            factoriel *= i;
+        }
+
+        return factoriel;
+    }
+
+    static void Main()
+    {
+        Console.Write("n=");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("k=");
+        int k = int.Parse(Console.ReadLine());
+
+        Console.WriteLine((CalculateFactoriel(n) * CalculateFactoriel(k)) / CalculateFactoriel(k - n));
+    }
+}
